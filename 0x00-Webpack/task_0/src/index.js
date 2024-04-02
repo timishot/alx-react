a7b035b20607706153d68d8e432490a0a9ef7677
Timishot <timidotcom58@gmail.com>
2024-04-03 00:19:28 +0100

    fix

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+$(document).ready(() => {
+  $('body').append(`
+    <p>Holberton Dashboard</p>
+    <p>Dashboard data for the students</p>
+    <p>Copyright - Holberton School</p>
+  `);
+});
