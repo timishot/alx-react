0b9ac0e55b5c7cfd937b41006c52cd7b58293872
Timishot <timidotcom58@gmail.com>
2024-05-15 15:22:35 +0100

    fix

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,2 +1,9 @@
-//working on immutable function
-//Still have to learn it
+const {List, Map} = require("immutable");
+let list = List.of(1,2,3);
+let list2 = list.push(4)
+console.log(list)
+console.log(list2)
+
+const alpha = Map({ a: 1, b: 2, c: 3, d: 4 });
+const time = alpha.map((i, v) => v.toUpperCase()).join();
+console.log(time)
